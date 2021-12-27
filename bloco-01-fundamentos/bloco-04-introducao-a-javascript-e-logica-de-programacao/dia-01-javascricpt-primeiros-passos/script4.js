6b15692774a48910c909e57532ea0c824ce1247c
Lucas Lima <robertolucas011@gmail.com>
2021-12-27 16:24:23 -0300

    Adiciona questão 4 e corrige o commit anterior.

--- bloco-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascricpt-primeiros-passos/script4.js
+++ bloco-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascricpt-primeiros-passos/script4.js
@@ -1,4 +1,4 @@
-//Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário
+//Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
 
 const num = 12;
 
