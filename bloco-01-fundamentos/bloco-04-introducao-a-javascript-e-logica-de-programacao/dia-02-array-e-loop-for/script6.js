8b49046e26f279a708d224bf3c8ed43634f9317c
Lucas Lima <robertolucas011@gmail.com>
2021-12-27 21:12:11 -0300

    Adiciona questão 9.

--- bloco-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/script6.js
+++ bloco-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/script6.js
@@ -1,6 +1,6 @@
 //Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numeros = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let total = 0;
 
 for (let i = 0; i < numeros.length; i += 1) {
