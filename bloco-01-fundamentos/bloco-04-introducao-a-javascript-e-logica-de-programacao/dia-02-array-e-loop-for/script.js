5ed89d922de16561a468efc3e322915c2ff3b4ad
Lucas Lima <robertolucas011@gmail.com>
2021-12-28 21:48:03 -0300

    Adiciona algoritmo à questão 1 do dia 2 de javacript.

--- bloco-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/script.js
+++ bloco-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/script.js
@@ -1,5 +1,10 @@
 //1 - Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
 
+//Alogoritimo:
+// 1 - Adicionar o array; 
+// 2 - Criar loop for para percorrer cada posição do array;
+// 3 - Adicionar console.log() para imprimir cada posição percorrida (numbers[i])
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (let i = 0; i < numbers.length; i += 1) {
