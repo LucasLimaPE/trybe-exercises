c0bccb2b7f6f6404318ccd14d0605d6c3fc70306
Lucas Lima <robertolucas011@gmail.com>
2021-12-29 00:14:15 -0300

    Adiciona algoritmo à questão 6 do dia 2 de javascript.

--- bloco-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/script6.js
+++ bloco-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/script6.js
@@ -1,5 +1,14 @@
 //Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
 
+//Algoritmo:
+// 1 - Usa o Array dos exercícios anteriores;
+// 2 - Declara uma variável que irá conter o número de ocorrências de números ímpares;
+// 3 - Cria uma estrutura de repetição que:
+      //Percorrerá todos os números do array;
+      //Declara estrutura condiciona (if/else), que irá comparar se o resto da divisão do número percorrido é diferente de zero (caracteruzizando um número ímpar), se for, a variável que conta as ocorrências será somada de 1.
+// 4 - Fora do for será declarada uma estrutura condicional que:
+        //Irá comparar se o número de ocorrências de números ímpares é igual a 0, se for imprimir-se-á na tela uma mensagem, se não, imprimir-se-á o valor da variável total. 
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let total = 0;
 
