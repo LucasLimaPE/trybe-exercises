489d0bf01deab501f63184893df1f6b437b5e019
Lucas Lima <robertolucas011@gmail.com>
2021-12-29 09:39:26 -0300

    Adiciona algoritmo à questão 9 do dia 2 de javascript.

--- bloco-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/script9.js
+++ bloco-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/script9.js
@@ -1,5 +1,10 @@
 //Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
 
+//Algoritmo:
+// 1 - Utiliza cósdigo de criação de array do exercícios 8.
+// 2 - Declara um um loop que vai percorrer cada  número do array criado.
+// 3 - Usa um console.log() para imprimir cada valor percorrido no for dividido por 2.
+
 /*Este código foi desenvolvido por Lucas Lima com auxílio do colega Guilherme Azevedo. Comparado com o código do gabarito e adicionado de alterações a partir dele. */
 
 let numbers = [];
