919e8524bee9a76d7c6ceb18b8b0f29b7686ecb5
Lucas Lima <robertolucas011@gmail.com>
2021-12-28 22:00:31 -0300

    Adiciona algoritmo à questão 2 do dia 2 de java script.

--- bloco-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/script2.js
+++ bloco-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/script2.js
@@ -1,4 +1,12 @@
-//Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+// 2 - Para o segundo exercício, some todos os valores contidos no array e imprima o resultado.
+
+//Algoritmo:
+//Usar array de números do exercício anterior;
+//Criar variável que vai armazenar o valor da soma;
+//Criar for (estrutura de repetição) que vai:
+    //Percorrer todo o array;
+    //A cada posição percorrida o valor da posição será adicionado à variável declarada anteriormente.
+    //Usar console.log para imprimir o valor total somado no loop na tela.
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
