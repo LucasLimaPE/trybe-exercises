c1b829096d0bd067fd2ddadd410e97e83724dfbc
Lucas Lima <robertolucas011@gmail.com>
2021-12-30 12:23:44 -0300

    Adiciona questão 1 do dia 4 de javascript.

---
+++ bloco-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script.js
@@ -0,0 +1,23 @@
+// 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console:
+
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
+
+//Algoritmo:
+// 1 - Usa-se o objeto dado no enunciado (info);
+// 2 - Usa-se um console.log(), para concatenar a mesagem de boas vindas.
+// 3 - Declara-se dentro do bloco de código a concatenação entre a mesagem de e o valor da chave personagem dentro do objeto info.
+
+
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+
+console.log("Boas vindas, " + info.personagem + ".");
