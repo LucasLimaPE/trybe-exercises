1f96a173665c063a0a98b6bdc9b3e9702032cd7d
Lucas Lima <robertolucas011@gmail.com>
2021-12-30 15:40:38 -0300

    Adiciona questão 8 do dia 4 de javascript e organiza uma parte do exercício 7.

---
+++ bloco-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/script8.js
@@ -0,0 +1,29 @@
+// 8 - Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: "Julia tem 2 livros favoritos".
+
+//Algoritmo:
+// 1 - Usa-se o mesmo objeto dos ecercícios anteriores;
+// 2 - Usa-se console.log() para imprimir a concatenação entre a frase e as informações dentro do objeto;
+// 3 - Atenta-se que por livrosFavoritos se tratar de uma string pode-se acessar o comprimento da chave.
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+  leitor.livrosFavoritos.push(
+    {
+        titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+        autor: 'JK Rowling',
+        editor: 'Rocco',
+      }
+  );
+
+  console.log('"Julia tem ' + leitor.livrosFavoritos.length + ' livros' + ' favoritos"');
