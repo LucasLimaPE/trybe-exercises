41af3926ef5e7d9fa6b5a5638c5282989888e6e5
LucasLimaPE <robertolucas011@gmail.com>
2022-01-13 11:19:12 -0300

    Modifica as concatenações para template literals e altera a declaração de variável dentro do escopo do else de let para const pois ela não terá seu valor reatribuído ou modificado.

--- bloco-01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ bloco-01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -7,10 +7,10 @@
 const testingScope = (escopo) => {
     if (escopo === true) {
       let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+      ifScope = `${ifScope} ótimo, fui utilizada no escopo!`;
       console.log(ifScope);
     } else {
-      let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
       console.log(elseScope);
     }
     // console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
