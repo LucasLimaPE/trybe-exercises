1d1959f9187b38673b0938b2bb3dddd2b4dd4fa9
LucasLimaPE <robertolucas011@gmail.com>
2022-01-14 13:08:09 -0300

    Cria o segunto test que irá verificar o retorno de sum(0, 0).

--- bloco-01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/sum.test.js
+++ bloco-01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/sum.test.js
@@ -5,3 +5,9 @@ describe ('Requisito 1', () => {
       expect(9).toEqual(sum(4, 5));
   });  
 });
+
+describe ('Requisito 2', () => {
+    it('A função recebe (0, 0) e retorna (0)', () => {
+        expect(0).toEqual(sum(0, 0));
+    });  
+  });
