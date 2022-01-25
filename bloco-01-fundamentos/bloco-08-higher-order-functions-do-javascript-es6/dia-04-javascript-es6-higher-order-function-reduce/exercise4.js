c249fb233396bc9506df675de7a1d12cb9548b80
LucasLimaPE <robertolucas011@gmail.com>
2022-01-25 16:32:40 -0300

    Adiciona questão 4 resolvida.

--- bloco-01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-function-reduce/exercise4.js
+++ bloco-01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-function-reduce/exercise4.js
@@ -64,7 +64,24 @@ const books = [
       releaseYear: 1928,
     },
   ];
-  
-  function longestNamedBook() {
-    // escreva seu código aqui
+/*
+Este código foi feito com base no gabarito da questão presenten no course, estava meio confuso em entender como iria acessar a propriedade name dentro do array e retornar o objeto que contém ela. Fiz alterações no código para poder receber qualquer array que seja de objetos e contenha a chave names.
+
+Algoritmo explicativo:
+1 - Usa-se o array books;
+2 - Declara-se a função (longesNamedBook) que recebe como parâmetro um array e retorna a aplicação de um reduce nesse array;
+3 - O reduce recebe como parâmetro uma função que recebe como parâmetro acc (acumulador) e curr (valor corrente) e abre o escopo da função de parâmetro;
+4 - Dentro do escopo existe um if que irá comparar se o tamanho/comprimento do valor percorrido no array ou seja do objeto na chave name (curr.name.length) é maior que o valor comprimento de da chave no acc que, como não foi passado um segundo parâmetro para o reduce, vai ser a primeira posiçao do array (acc.name.length), se for maior, irá retornar o curr.
+5 - Caso não entre nesse if irá retornar acc que, como foi dito anteriormente, é a primeira posição do array.
+*/
+
+
+const longestNamedBook = (array) => array.reduce((acc, curr) => {
+  if (curr.name.length > acc.name.length) {
+    return curr;
   }
+  return acc;
+});
+
+  
+console.log(longestNamedBook(books));
