b9a98977137775e166039c24e7af758501f93aa5
LucasLimaPE <robertolucas011@gmail.com>
2022-01-28 21:07:29 -0300

    Adiciona questão 2 resolvida.

--- bloco-01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise2.js
+++ bloco-01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise2.js
@@ -2,3 +2,8 @@
 2 - Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos.
 */
 
+const sommation = (...args) => {
+ return args.reduce((acc, curr) => acc + curr, 0);
+}
+
+console.log(sommation(3, 4, 5, 6, 7, 8, 9, 10));
