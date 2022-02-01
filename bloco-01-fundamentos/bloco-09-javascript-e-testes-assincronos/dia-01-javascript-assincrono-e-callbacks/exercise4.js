8745f38eececc0d53609e92fa183935355877b59
LucasLimaPE <robertolucas011@gmail.com>
2022-02-01 10:13:58 -0300

    Adiciona exercício 4 resolvido.

--- bloco-01-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/exercise4.js
+++ bloco-01-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/exercise4.js
@@ -10,5 +10,6 @@ const getMarsTemperature = () => {
 };
 
 // crie a função sendMarsTemperature abaixo
+const sendMarsTemperature = () => setTimeout(() => console.log(`Mars temperature is: ${getMarsTemperature()} degree Celsius`), `${messageDelay()}`);
 
 sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
