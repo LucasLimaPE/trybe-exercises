d4551e073681d31cdfb14eba2783adb08cd65f0c
LucasLimaPE <robertolucas011@gmail.com>
2022-02-12 11:40:11 -0300

    Adiciona exercício 9.

--- bloco-01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/script.js
+++ bloco-01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/script.js
@@ -171,3 +171,18 @@ function createHolidayButton(buttonName) {
   };
   
   newTaskDiv('green');
+
+  function setTaskClass() {
+    let selectedTask = document.getElementsByClassName('task selected');
+    let myTasks = document.querySelector('.task');
+  
+    myTasks.addEventListener('click', function(event) {
+      if (selectedTask.length === 0) {
+        event.target.className = 'task selected';
+      } else {
+        event.target.className = 'task';
+      }
+    });
+  };
+  
+  setTaskClass();
