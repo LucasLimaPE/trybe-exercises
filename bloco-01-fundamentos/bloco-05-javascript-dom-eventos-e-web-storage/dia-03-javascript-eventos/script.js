5a5d7813f3414e6d4664f52ca31325cca90a1c9d
LucasLimaPE <robertolucas011@gmail.com>
2022-02-12 10:01:18 -0300

    Adiciona exercício 4.

--- bloco-01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/script.js
+++ bloco-01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/script.js
@@ -73,3 +73,16 @@ function createHolidayButton(buttonName) {
   };
   
   displayHolidays();
+
+  function createFridayButton(buttonName) {
+    let buttonContainer = document.querySelector('.buttons-container');
+    let newButton = document.createElement('button');
+    let newButtonID = 'btn-friday';
+  
+    newButton.innerHTML = buttonName;
+    newButton.id = newButtonID;
+    buttonContainer.appendChild(newButton);
+  };
+  
+  createFridayButton('Sexta-feira');
+  
