e9284de8edc9b6f34dde6255d99c185de73797e7
LucasLimaPE <robertolucas011@gmail.com>
2022-02-12 11:31:50 -0300

    Adiciona exercício 5.

--- bloco-01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/script.js
+++ bloco-01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/script.js
@@ -85,4 +85,41 @@ function createHolidayButton(buttonName) {
   };
   
   createFridayButton('Sexta-feira');
+
+  function displayFridays(fridaysArray) {
+    let getFridayButton = document.querySelector('#btn-friday');
+    let fridays = document.getElementsByClassName('friday');
+    let newFridayText = 'SEXTOU o/';
   
+    getFridayButton.addEventListener('click', function() {
+    for (let index = 0; index < fridays.length; index += 1) {
+      if (fridays[index].innerHTML !== newFridayText) {
+          fridays[index].innerHTML = newFridayText;
+      } else {
+          fridays[index].innerHTML = fridaysArray[index];
+        }
+      }
+    })
+  };
+  
+  let dezFridays = [ 4, 11, 18, 25 ];
+  displayFridays(dezFridays);
+
+  function displayFridays(fridaysArray) {
+    let getFridayButton = document.querySelector('#btn-friday');
+    let fridays = document.getElementsByClassName('friday');
+    let newFridayText = 'SEXTOU o/';
+  
+    getFridayButton.addEventListener('click', function() {
+    for (let index = 0; index < fridays.length; index += 1) {
+      if (fridays[index].innerHTML !== newFridayText) {
+          fridays[index].innerHTML = newFridayText;
+      } else {
+          fridays[index].innerHTML = fridaysArray[index];
+        }
+      }
+    })
+  };
+  
+  let dezFridays = [ 4, 11, 18, 25 ];
+  displayFridays(dezFridays);
